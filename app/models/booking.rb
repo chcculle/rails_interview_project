46287c94aaae52e43e26bcbc691de914c35e2eaa
Cathy Cullen <cathy@softwareoptions.com>
2015-04-24 15:22:47 -0500

    find available parking spaces for start end time

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,8 @@
 class Booking < ActiveRecord::Base
   belongs_to :listing
+
+  def self.listings_booked_during_timeframe(inquiry_start, inquiry_end)
+    Booking.where.not('time_start >= ? OR  time_end <= ?', inquiry_end, inquiry_start).pluck(:listing_id)
+  end
+
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,29 @@
 class Listing < ActiveRecord::Base
   has_many :bookings
+
+  def self.available_listings(inquiry_start, inquiry_end)
+
+    # determine how many spots per listing are booked during this timeframe
+    spots_taken_per_listing = Hash.new 0
+    Booking.listings_booked_during_timeframe(inquiry_start, inquiry_end).each do |listing|
+      spots_taken_per_listing[listing] += 1
+    end
+
+     #collect available listings.  start with those not booked during this timeframe
+    available_listings = Listing.where.not(:id => spots_taken_per_listing.keys).to_a
+
+    #for those listings that are booked during this timeframe determine if there is room and add to available listings
+    Listing.where(:id => spots_taken_per_listing.keys).each do |listing|
+      if spots_taken_per_listing[listing.id] < listing.available
+        available_listings << listing
+      end
+    end
+    available_listings
+  end
+
+
 end
+
+
+
+
